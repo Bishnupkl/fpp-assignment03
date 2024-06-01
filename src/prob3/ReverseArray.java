76a933d2a1037c72a61eec5c95a6f198bfc160a1
bishnu <bishnup212@gmail.com>
2024-06-01 14:46:34 -0500

    Solved problem 3

---
+++ src/prob3/ReverseArray.java
@@ -0,0 +1,28 @@
+package prob3;
+
+import java.util.Arrays;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+
+        int[] initialArray = {1, 3, 4, 7, 9, 13};
+        System.out.println(STR."Array Before Reversing:\{Arrays.toString(initialArray)}");
+        reverseArray(initialArray);
+        System.out.println(STR."Array After Reversing:\{Arrays.toString(initialArray)}");
+    }
+
+    public static void reverseArray(int[] array) {
+        reverseArray(array, 0, array.length - 1);
+    }
+
+    private static void reverseArray(int[] array, int i, int j) {
+        if (i >= j) {
+            return;
+        }
+        int tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
+        reverseArray(array, ++i, --j);
+
+    }
+}
