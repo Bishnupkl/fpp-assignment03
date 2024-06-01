c02c9c5f9f875edf2c1fe64f8d73607370101d9b
Kai <40851151+Kheang1409@users.noreply.github.com>
2024-06-01 16:47:37 -0500

    update prog3
    so it goes with Test

--- src/Prog3.java
+++ src/Prog3.java
@@ -9,18 +9,19 @@ public class Prog3 {
         System.out.println(STR."Array After Reversing:\{Arrays.toString(initialArray)}");
     }
 
-    public static void reverseArray(int[] array) {
-        reverseArray(array, 0, array.length - 1);
+    public static int[] reverseArray(int[] array) {
+        reverseArrayGenerator(array, 0, array.length - 1);
+        return array;
     }
 
-    private static void reverseArray(int[] array, int i, int j) {
+    private static void reverseArrayGenerator(int[] array, int i, int j) {
         if (i >= j) {
             return;
         }
         int tmp = array[i];
         array[i] = array[j];
         array[j] = tmp;
-        reverseArray(array, ++i, --j);
+        reverseArrayGenerator(array, ++i, --j);
 
     }
 }
