0cca916ed3d7fc450d8f721139f66c8edf420fa7
Kai <40851151+Kheang1409@users.noreply.github.com>
2024-06-01 15:18:54 -0500

    Update Power.java
    modified power by negative number

--- src/prob1/Power.java
+++ src/prob1/Power.java
@@ -5,20 +5,22 @@ import java.util.Scanner;
 public class Power {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println(("Enter the base number"));
+        System.out.print(("Enter the base number: "));
         double x = sc.nextDouble();
-        System.out.println(("Enter the exponent number"));
+        System.out.print(("Enter the exponent number:"));
         int n = sc.nextInt();
         System.out.println(STR."The \{n} Power of \{x} is \{power(x, n)}");
-
-
     }
 
     static double power(double x, int n) {
+        if (n < 0) return Math.pow(x, n);
         if (n == 0) {
             return 1;
-        } else {
-            return x * power(x, n - 1);
+        } else {  // Negative power case
+            if (x == 0) {  // Handle division by zero for negative base and exponent
+                throw new ArithmeticException("Cannot raise zero to a negative power");
+            }
+            return 1.0 / power(x, -n);
         }
     }
 }
