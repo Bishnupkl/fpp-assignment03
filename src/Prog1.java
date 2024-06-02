ea80a9dcd59a59c93f21c1f1b62d2fb10039e674
Kai <40851151+Kheang1409@users.noreply.github.com>
2024-06-02 16:10:37 -0500

    prog1
    update conflicted

--- src/Prog1.java
+++ src/Prog1.java
@@ -7,14 +7,17 @@ public class Prog1 {
         double x = sc.nextDouble();
         System.out.print(("Enter the exponent number:"));
         int n = sc.nextInt();
-        System.out.println(STR."The \{n} Power of \{x} is \{power(x, n)}");
+        System.out.println(STR."The \{n} Power of \{x} is \{power(x, n)} compare to Math\{Math.pow(x, n)}" );
     }
 
     public static double power(double x, int n) {
-        if (n < 0) return Math.pow(x, n);
         if (n == 0) {
             return 1;
-        } else {  // Negative power case
+        }
+        else if(n>0){
+            return x * power(x, n-1);
+        }
+        else {  // Negative power case
             if (x == 0) {  // Handle division by zero for negative base and exponent
                 throw new ArithmeticException("Cannot raise zero to a negative power");
             }
